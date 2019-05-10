b2652c5c1d3d3dfa6647f3caa0875148d339a759
C3NZ <cenz@cenz.io>
2019-05-10 15:57:04 -0700

    [feat] filter out the columns that we don't want'

--- regression.py
+++ regression.py
@@ -1,3 +1,25 @@
+import pandas as pd
+
 from clean import get_nba_df
 
-print(get_nba_df())
+
+def filter_cols(df: pd.DataFrame) -> tuple:
+    # Columns we want to remove
+    unwanted_cols = ["Year", "Pos", "Age", "Tm", "blanl", "blank2", "OWS", "DWS"]
+
+    # Target column we'd like
+    target_col = ["WS"]
+    nba_stats = df.drop(columns=unwanted_cols)
+    nba_ws = df[target_col]
+
+    return nba_stats, nba_ws
+
+
+def main():
+    nba_stats, nba_ws = filter_cols(get_nba_df())
+    print(nba_stats)
+    print(nba_ws)
+
+
+if __name__ == "__main__":
+    main()
